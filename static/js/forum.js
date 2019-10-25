4fc76cb444e14ffb638e0243ac1ab90cd9d489ad
guyraz89 <gnewr89@gmail.com>
2019-10-25 21:56:22 +0300

    1

--- static/js/forum.js
+++ static/js/forum.js
@@ -12,7 +12,7 @@ function loadCards() {
             breeds = JSON.parse(response).sort();
             for (let i = 0; i < breeds.length; i++) {
                 //$('.row').append('<div class="column"> <div class="card"><h3>Card 1</h3><p>' + breeds[i] + '</p><p>Some text</p> </div></div>')        
-                $('.posts-container').append('<a href='+ breeds[i] + '><div class="single-post-container"><div class="dog-image"><img src="../static/images/dog.png" class="rounded-circle" alt="Cinque Terre"></div><div class="dog-name"><h1>' + breeds[i] + '</h1></div></div></a>')
+                $('.posts-container').append('<a href='+ toTitleCase(breeds[i]) + '><div class="single-post-container"><div class="dog-image"><img src="../static/images/dog.png" class="rounded-circle" alt="Cinque Terre"></div><div class="dog-name"><h1>' + toTitleCase(breeds[i]) + '</h1></div></div></a>')
             // TODO: 1. replace the current cards with the new one
             //       2. add photes of dogs
             }
@@ -22,4 +22,12 @@ function loadCards() {
         }
     });
     return breeds;
+}
+
+function toTitleCase(text) {
+    text = text.toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+    return text;
 }
