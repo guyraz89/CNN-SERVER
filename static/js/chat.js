ecbd476b76c86d220030b88396827acbf61dd47c
Nati_Shchiglik <natishch94@gmail.com>
2019-10-26 20:03:54 +0300

    finish js chat

--- static/js/chat.js
+++ static/js/chat.js
@@ -12,13 +12,31 @@ $(function(){
     
     firebase.initializeApp(fbg);
     var database = firebase.database();
-    database.ref('dogbreed').on('value', gotData, gotError);
+
+    let url = window.location.href;
+    urlArr = url.split('/');
+    let dogbreed = urlArr[urlArr.length-1];
+
+    $('#chat_name').text(toTitleCase(dogbreed) + ' Chat');
+
+    var dogRef = database.ref().child('dogBreed/').child(dogbreed).child('/messages');
+    dogRef.on('value', gotData, gotError);
 
     function gotData(data){
-        console.log(data);
+        $('#chat_body').html('');
         var messages = data.val();
         var keys = Object.keys(messages);
-        console.log(keys);
+        var myUid = firebase.auth().currentUser.uid;
+        for( var i=0; i < keys.length; i++){
+            var k = keys[i];
+            if(myUid == messages[k].uid){
+                console.log('my '+messages[k].time);
+                localMessage("../static/images/avatar.png", messages[k].body, messages[k].time);
+            }else{
+                console.log('Not my '+messages[k].time);
+                externalMessage("../static/images/avatar.png", messages[k].body, messages[k].time);
+            }
+        }
     }
 
     function gotError(error){
@@ -26,27 +44,23 @@ $(function(){
     }
 
     $('#add_btn').click(() => {
-        let url = window.location.href;
-        urlArr = url.split('/');
-        let dogbreed = urlArr[urlArr.length-1];
         let body = $('#chat_input').val();
-        let user = firebase.auth().currentUser;
+        var user = firebase.auth().currentUser;
         if (user) {
             var name = user.displayName;
             var uid = user.uid;
         }
-        console.log('dogbreed: '+dogbreed+'\nuid: '+uid+'\nname: '+name+'\nbody: '+body);
-        writeNewMessage(dogbreed,uid,name,body);
-        localMessage("../static/images/avatar.png", "Hi my name is guy", "timestamp")// Working.. need to set image of user and timestamp
+        var d = new Date().toDateString();
+        writeNewMessage(dogbreed,uid,body,d);
+        $('#chat_input').val('');
     });
 
-    function writeNewMessage(dogBreed,uid, username, body) {
-        console.log(database);
+    function writeNewMessage(dogBreed,uid, body, date) {
         // A post entry.
         var postData = {
-          author: username,
           uid: uid,
           body: body,
+          time: date
         };
         // Get a key for a new Post.
       var newMessageKey = database.ref().child('dogBreed').child(dogBreed).child('messages').push().key;
@@ -59,21 +73,19 @@ $(function(){
     }
 });
 
-function readMessages(dogBreed){
-    database.ref().child('dogBreed').child(dogBreed).on('value',
-        function(snapshot){
-            snapshot.forEach(function(childSnapshot){
-                var childKey = childSnapshot.key;
-                var childData = childSnapshot.val();
-            });
-        });
-}
-
 function localMessage(imgSrc, msg, timestamp) {
     $('#chat_body').append('<div class="d-flex justify-content-start mb-4"> <div class="img_cont_msg"> <img src="' + imgSrc + '" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + msg + '<span class="msg_time">' + timestamp + '</span></div></div>');
 }
 
 function externalMessage(imgSrc, msg, timestamp) {
     $('#chat_body').append('<div class="d-flex justify-content-end mb-4"> <div class="img_cont_msg"> <img src="' + imgSrc + '" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + msg + '<span class="msg_time">' + timestamp + '</span></div></div>');
+}
+
+function toTitleCase(text) {
+    text = text.toLowerCase()
+        .split('_')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+    return text;
 }
 
